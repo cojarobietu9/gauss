b9aa54ba83984d2e0292fd3a040c966ecc5eaa18
Imperator-Marius-Wlodarius-IV <mariusz.kossakowski1@gmail.com>
2023-12-22 12:06:56 +0100

    commit poprawkowy

--- src/gauss.c
+++ src/gauss.c
@@ -1,16 +1,43 @@
 #include "gauss.h"
-
+#include <stdio.h>
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * mala zaba
-		 */
-a=a;
-b=b;
-cecylia ameba pieprz;
-		return 0;
-}
+ 
+Zwraca 0 - elimnacja zakonczona sukcesem
+Zwraca 1 - macierz osobliwa - dzielenie przez 0
+*/
+int eliminate(Matrix mat, Matrixb, int v){
 
+         int i;
+         int n = mat[v][v];
+         int s = v;
+         for (i=v;i++;i<n)
+                {
+                        if(mat[v][i]>n)
+                                {
+                                        n = mat[v][i];
+                                        s = i;
+                                }
+                }
+        if (s==v && n == 0)
+        {
+                return 1;
+        }
+ else {
+                int a;
+                for (i=0;i++;i<n)
+                {
+                        a = mat[v][i];
+                        mat[v][i] = mat[s][i];
+                        mat[s][i] = mat[v][i];
+                }
+                for (i=1;i++;i<n)
+                        {
+                                for (j=0;j++;j<n)
+                                        {
+                                                mat[i][j]/=mat[v][v];
+                                        }
+                        }
+                return eliminate(Matrix mat, Matrixb, v-1)
+        }
+                return 0;
+}
